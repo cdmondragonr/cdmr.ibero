6704a1a67f2b38098def41cbc0aa27ac5c4d5dcc
Cristian Mondragon <82355137+cdmondragonr@users.noreply.github.com>
2024-12-01 16:56:30 -0500

    Add files via upload

---
+++ NoSQLdb/crudmongodb/src/main/java/com/ibero/crudmongodb/service/PersonaService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ibero.crudmongodb.service;
+
+import com.ibero.crudmongodb.model.Persona;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author cdmrr
+ */
+public interface PersonaService {
+    
+    List<Persona> GetAllPersonas();
+    Optional<Persona>getPersonaById(Long id);
+    Persona savePersona(Persona persona);
+    void deletePersonaById(Long id);
+}
---
+++ NoSQLdb/crudmongodb/src/main/java/com/ibero/crudmongodb/service/PersonaServiceImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ibero.crudmongodb.service;
+
+import com.ibero.crudmongodb.api.PersonaRepository;
+import com.ibero.crudmongodb.model.Persona;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author cdmrr
+ */
+public class PersonaServiceImpl implements PersonaService{
+    
+    @Autowired
+    private PersonaRepository personaRepository;
+    
+    @Override
+    public List<Persona> GetAllPersonas() {
+        return personaRepository.findAll();
+    }
+
+    @Override
+    public Optional<Persona> getPersonaById(Long id) {
+        return personaRepository.findById(id);
+    }
+
+    @Override
+    public Persona savePersona(Persona persona) {
+        return personaRepository.save( persona);
+    }
+
+    @Override
+    public void deletePersonaById(Long id) {
+        personaRepository.deleteById(id);
+    }
+    
+}
